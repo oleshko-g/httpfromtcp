2cf1ceded2c61073c192316506f21266c554e7ef
Gennady Oleshko <gennady@oleshko-g.ru>
2025-03-22 00:16:03 +0300

    read 8 byte chunks

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+const (
+	messages = "./messages.txt"
+)
+
 func main() {
-	print("I home I get the job!")
+	file, err := os.Open(messages)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+	}
+	defer file.Close()
+
+	buf := make([]byte, 8)
+
+	for {
+		n, err := file.Read(buf)
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			file.Close()
+			os.Exit(1)
+		}
+
+		fmt.Fprintf(os.Stdout, "read: %s\n", buf[0:n])
+	}
 }
