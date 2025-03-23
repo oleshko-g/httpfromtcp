2efbfdd62dab5644a419c65f8ff055d70c49e466
Gennady Oleshko <gennady@oleshko-g.ru>
2025-03-23 19:32:07 +0300

    channel refactor

--- main.go
+++ main.go
@@ -10,36 +10,50 @@ const (
 	messages = "./messages.txt"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lines := make(chan string)
+	buf := make([]byte, 8)
+	go func() {
+		for line := make([]byte, 0); ; {
+			n, err := f.Read(buf)
+			if err == io.EOF {
+				if len(line) != 0 {
+					lines <- string(line)
+				}
+				f.Close()
+				close(lines)
+				break
+			}
+
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				f.Close()
+				os.Exit(1)
+			}
+
+			for _, r := range buf[:n] {
+				if r == '\n' {
+					lines <- string(line)
+					line = line[:0]
+					continue
+				}
+				line = append(line, r)
+			}
+		}
+	}()
+	return lines
+}
+
 func main() {
 	file, err := os.Open(messages)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
-	defer file.Close()
 
-	buf := make([]byte, 8)
-	for line := make([]byte, 0); ; {
-		n, err := file.Read(buf)
-		if err == io.EOF {
-			if len(line) != 0 {
-				fmt.Fprintf(os.Stdout, "read: %s\n", string(line))
-			}
-			break
-		}
+	lines := getLinesChannel(file)
 
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			file.Close()
-			os.Exit(1)
-		}
-
-		for _, r := range buf[:n] {
-			if r == '\n' {
-				fmt.Fprintf(os.Stdout, "read: %s\n", string(line))
-				line = line[:0]
-				continue
-			}
-			line = append(line, r)
-		}
+	for v := range lines {
+		fmt.Fprintf(os.Stdout, "read: %s\n", v)
 	}
+
 }
