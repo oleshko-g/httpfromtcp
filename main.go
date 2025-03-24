e54846b623fff3983128ec0ef5c9ecc84c443194
Gennady Oleshko <gennady@oleshko-g.ru>
2025-03-25 00:30:22 +0300

    implement tcp listener

--- main.go
+++ main.go
@@ -3,57 +3,81 @@ package main
 import (
 	"fmt"
 	"io"
+	"net"
 	"os"
 )
 
 const (
-	messages = "./messages.txt"
+	network = "tcp"
 )
 
-func getLinesChannel(f io.ReadCloser) <-chan string {
+func getLinesChannel(r io.ReadCloser) <-chan string {
 	lines := make(chan string)
-	buf := make([]byte, 8)
+
 	go func() {
+		defer close(lines)
+		buf := make([]byte, 8)
 		for line := make([]byte, 0); ; {
-			n, err := f.Read(buf)
+			n, err := r.Read(buf)
+			if n > 0 {
+				for _, r := range buf[:n] {
+					if r == '\n' {
+						lines <- string(line)
+						line = line[:0]
+						continue
+					}
+					line = append(line, r)
+				}
+			}
+
 			if err == io.EOF {
 				if len(line) != 0 {
 					lines <- string(line)
 				}
-				f.Close()
-				close(lines)
+				err := r.Close()
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err)
+				}
 				break
 			}
 
 			if err != nil {
 				fmt.Fprintln(os.Stderr, err)
-				f.Close()
-				os.Exit(1)
-			}
-
-			for _, r := range buf[:n] {
-				if r == '\n' {
-					lines <- string(line)
-					line = line[:0]
-					continue
+				err := r.Close()
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err)
 				}
-				line = append(line, r)
+				os.Exit(1)
 			}
 		}
 	}()
+
 	return lines
 }
 
 func main() {
-	file, err := os.Open(messages)
+	listener, err := net.Listen(network, "127.0.0.1:42069")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
+	defer func() {
+		err := listener.Close()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}()
 
-	lines := getLinesChannel(file)
-
-	for v := range lines {
-		fmt.Fprintf(os.Stdout, "read: %s\n", v)
+	for {
+		conn, err := listener.Accept()
+		fmt.Println("A connection has been accepted")
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		lines := getLinesChannel(conn)
+		for v := range lines {
+			fmt.Fprintf(os.Stdout, "%s\n", v)
+		}
+		fmt.Println("A connection has been closed")
 	}
 
 }
